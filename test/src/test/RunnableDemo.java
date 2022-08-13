abeb86dda43132c36d62be1abe2308e6ebccfa6c
ameenulhaseeb <ameenulhaseeb@gmail.com>
2022-08-13 14:25:59 +0530

    runnable ne changes

--- test/src/test/RunnableDemo.java
+++ test/src/test/RunnableDemo.java
@@ -7,7 +7,24 @@ class A implements Runnable{
 		System.out.println("Hi");
 		}
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(5000000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
+
+class B implements Runnable{
+
+	@Override
+	public void run() {
+		for(int i=1;i<=5;i++) {
+		System.out.println("Hello");
+		}
+		try {
+			Thread.sleep(500000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -17,8 +34,18 @@ class A implements Runnable{
 }
 public class RunnableDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		A obj1 = new A();
+		B obj2 = new B();
+		
+		Thread t1 = new Thread(obj1);
+		Thread t2 = new Thread(obj2);
+		
+		t1.start();
+		Thread.sleep(1000);
+		t2.start();
+		
 
 	}
 
