1697d2f69c57c31dc2c5a1fe14ee3d4431d189b2
ameenulhaseeb <ameenulhaseeb@gmail.com>
2022-08-27 22:00:08 +0530

    Condition

---
+++ test/src/test/ConditionDemo.java
@@ -0,0 +1,14 @@
+package test;
+
+public class ConditionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+if(1<2) {
+	System.out.println("First");
+}else {
+	System.out.println("Second");
+}
+	}
+
+}
