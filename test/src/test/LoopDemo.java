c6b7b97ed76bbf182643cf9d976b191cfe9230e6
ameenulhaseeb <ameenulhaseeb@gmail.com>
2022-08-27 21:52:33 +0530

    Loop Demo

---
+++ test/src/test/LoopDemo.java
@@ -0,0 +1,12 @@
+package test;
+
+public class LoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=1;i<=6;i++) {
+	System.out.println("Hi");
+}
+	}
+
+}
